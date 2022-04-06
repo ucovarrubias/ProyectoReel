ba12b9c4a78826848b2ffbb06dceb5a2a847d1a9
Almechic12 <ediithcarbherre25@gmail.com>
2022-04-05 23:11:30 -0700

    Segun funciona los botones del login

--- app/src/main/java/utiak/covarrubias/proyectoreel/MainActivity.kt
+++ app/src/main/java/utiak/covarrubias/proyectoreel/MainActivity.kt
@@ -12,7 +12,23 @@ class MainActivity : AppCompatActivity() {
 
         val button: Button = findViewById(R.id.btn_ingresar)
         button.setOnClickListener{
-            var intent: Intent = Intent(this, )
+            var intent: Intent = Intent(this, Menu::class.java)
+            startActivity(intent)
+        }
+        val buttonR: Button=findViewById(R.id.btn_registrarse)
+        buttonR.setOnClickListener{
+            var intent: Intent = Intent(this, registrarse::class.java)
+            startActivity(intent)
+        }
+        val buttonOU: Button=findViewById(R.id.btn_olvidarC)
+        buttonR.setOnClickListener{
+            var intent: Intent = Intent(this, recuperar_contrasenia::class.java)
+            startActivity(intent)
+        }
+        val buttonOC: Button=findViewById(R.id.btn_olvidarU)
+        buttonR.setOnClickListener{
+            var intent: Intent = Intent(this, Usuario_Recuperar::class.java)
+            startActivity(intent)
         }
 
 
