050f918ff741098265433ae3b4c73513c82fa960
Almechic12 <ediithcarbherre25@gmail.com>
2022-04-06 04:16:03 -0700

    layout de filtros

---
+++ app/src/main/java/utiak/covarrubias/proyectoreel/filtro.kt
@@ -0,0 +1,11 @@
+package utiak.covarrubias.proyectoreel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class filtro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filtro)
+    }
+}
