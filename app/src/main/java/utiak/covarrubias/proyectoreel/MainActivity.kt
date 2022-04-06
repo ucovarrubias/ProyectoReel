beef40506cd53524575b127107b2e1379809be2e
Almechic12 <ediithcarbherre25@gmail.com>
2022-04-05 23:59:39 -0700

    Este es el bueno

--- app/src/main/java/utiak/covarrubias/proyectoreel/MainActivity.kt
+++ app/src/main/java/utiak/covarrubias/proyectoreel/MainActivity.kt
@@ -10,23 +10,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.btnlogin)
+
+        val buttonOc: Button=findViewById(R.id.btn_olvidarC) as Button
+        val button: Button = findViewById(R.id.btnlogin) as Button
+        val buttonR: Button=findViewById(R.id.btnregistrarse) as Button
+        val buttonOu: Button=findViewById(R.id.btn_olvidarU) as Button
+
+
         button.setOnClickListener{
             var intent: Intent = Intent(this, Menu::class.java)
             startActivity(intent)
         }
-        val buttonR: Button=findViewById(R.id.btnregistrarse)
+
         buttonR.setOnClickListener{
             var intent: Intent = Intent(this, registrarse::class.java)
             startActivity(intent)
         }
 
-        val buttonOc: Button=findViewById(R.id.btn_olvidarC)
+
         buttonR.setOnClickListener{
             var intent: Intent = Intent(this, Contra_Recuperar::class.java)
             startActivity(intent)
         }
-        val buttonOu: Button=findViewById(R.id.btn_olvidarU)
+
         buttonR.setOnClickListener{
             var intent: Intent = Intent(this, Usuario_Recuperar::class.java)
             startActivity(intent)
