6c95f1ec810a51088d66e351ba4831899946db9c
Almechic12 <ediithcarbherre25@gmail.com>
2022-04-05 23:04:16 -0700

    Ajustes en el login

---
+++ app/src/main/java/utiak/covarrubias/proyectoreel/Contra_Recuperar.kt
@@ -0,0 +1,11 @@
+package utiak.covarrubias.proyectoreel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Contra_Recuperar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contra_recuperar)
+    }
+}
--- app/src/main/java/utiak/covarrubias/proyectoreel/MainActivity.kt
+++ app/src/main/java/utiak/covarrubias/proyectoreel/MainActivity.kt
@@ -1,14 +1,19 @@
 package utiak.covarrubias.proyectoreel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: androidx.appcompat.widget.AppCompatButton
+        val button: Button = findViewById(R.id.btn_ingresar)
+        button.setOnClickListener{
+            var intent: Intent = Intent(this, )
+        }
 
 
 
